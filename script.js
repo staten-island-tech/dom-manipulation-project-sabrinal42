59261b90248bdd107fd0b215b316cfd235c58ca0
sabrinal42 <78661937+sabrinal42@users.noreply.github.com>
2022-11-07 13:59:53 -0500

    j

---
+++ script.js
@@ -0,0 +1,6 @@
+const DOMSelectors = {
+  button: document.getElementById("button"),
+  text: document.querySelector("text"),
+  meow: document.querySelectorAll("meow"),
+};
+console.log(DOMSelectors.meow);
