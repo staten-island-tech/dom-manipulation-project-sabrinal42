6f627be2f773ecd67ba297aacf2c2f057f2191f0
sabrinal42 <78661937+sabrinal42@users.noreply.github.com>
2022-11-14 14:00:25 -0500

    s

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const DOMSelectors = {
   button: document.getElementById("button"),
-  text: document.querySelector("text"),
-  meow: document.querySelectorAll("meow"),
+  text: document.querySelector("#text"),
+  box: document.getElementById("big-black-box"),
+  points: document.querySelectorAll(".point"),
 };
-console.log(DOMSelectors.meow);
+
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.innerHTML = "This is now a big red box";
+  text.style.fontSize = "40px";
+}
+
+DOMSelectors.button.addEventListener("click", function () {
+  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+});
