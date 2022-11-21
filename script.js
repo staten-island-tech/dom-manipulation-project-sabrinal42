c8524ddf42e53726135da5a94d920925dcff7089
sabrinal42 <78661937+sabrinal42@users.noreply.github.com>
2022-11-21 14:04:48 -0500

    sdasda

--- script.js
+++ script.js
@@ -5,30 +5,15 @@ const DOMSelectors = {
   div: document.getElementById("container"),
   results: document.getElementById("results"),
 };
-
+//get values
+//create user object from values
+//create HTML string and inputadjacent HTML
+//queryALL remove buttons
+//forEach button ad event listener
+//clear fields
 const eraList = [
-  "<P>${user.name}, you are in your midnights era</P>",
+  `<P>${user.name}, you are in your midnights era</P>`,
   "<P>${user.name}, you are in your lover era</P>",
   "<P>${user.name}, you are in your fearless era</P>",
   "<P>${user.name}, you are in your reputation era</P>",
 ];
-
-enter.addEventListener("click", displayera);
-var count = 0;
-function displayera() {
-  results.innerHTML = eraList[count];
-  count++;
-  if (count == eraList.length) {
-    count = 0;
-  }
-}
-
-clear.addEventListener("click", clearboth);
-function clearboth() {
-  DOMSelectors.name.value = "";
-}
-
-trash.addEventListener("trash", trashy);
-function trashy() {
-  DOMSelectors.results.value = "";
-}
